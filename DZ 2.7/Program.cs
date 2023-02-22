bfe35d14e3ca1398e78a5a377ef27776e58cf3e4
Anikin Stanislav <saulgoodman7777@yandex.ru>
2023-02-22 11:55:41 +0300

    Add project files.

---
+++ DZ 2.7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
