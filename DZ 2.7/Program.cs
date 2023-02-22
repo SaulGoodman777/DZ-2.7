cce734690f53299b1ada9877d8a4dd3dcbb87d04
Anikin Stanislav <saulgoodman7777@yandex.ru>
2023-02-22 12:30:15 +0300

    First check

--- DZ 2.7/Program.cs
+++ DZ 2.7/Program.cs
@@ -10,6 +10,36 @@ namespace DZ_2._7
     {
         static void Main(string[] args)
         {
+            String fullName = "Anikin Stanislav Gennadyevich";
+            Byte yourAge = 36;
+            String yourEmail = "saulgoodman7777@yandex.ru";
+            Double assessmentPrograming = 89.7;
+            Double assessmentMath = 78.8;
+            Double assessmentPhysics = 92.3; // Конец первого Задания
+
+            Console.WriteLine("Full name: " + fullName);
+            Console.WriteLine("Your Age: " + yourAge);
+            Console.WriteLine("Your Email: " + yourEmail);
+            Console.WriteLine("Points Program: " + assessmentPrograming);
+            Console.WriteLine("Points Mahth: " + assessmentMath);
+            Console.WriteLine("Points Physics: " + assessmentPhysics);
+            Console.ReadKey();
+
+            //Начало второго задания
+            Double a = assessmentPrograming;
+            Double b = assessmentMath;
+            Double c = assessmentPhysics;
+
+            Double totalPoints = a + b + c;
+            Console.WriteLine("Total Point: " + totalPoints);
+            Console.ReadKey();
+
+            Double averagePoints = (a + b + c) / 3;
+            Console.WriteLine("Average points: " + string.Format("{0:F2}", averagePoints));
+            Console.ReadKey();
+
+
+
 
         }
     }
