5884e289282611bee7bdc4be69040b41550e5f16
Anikin Stanislav <saulgoodman7777@yandex.ru>
2023-02-24 14:26:53 +0300

    new

--- DZ 2.7/Program.cs
+++ DZ 2.7/Program.cs
@@ -10,32 +10,35 @@ namespace DZ_2._7
     {
         static void Main(string[] args)
         {
+
             String fullName = "Anikin Stanislav Gennadyevich";
             Byte yourAge = 36;
             String yourEmail = "saulgoodman7777@yandex.ru";
             Double assessmentPrograming = 89.7;
             Double assessmentMath = 78.8;
-            Double assessmentPhysics = 92.3; // Конец первого Задания
-
-            Console.WriteLine("Full name: " + fullName);
-            Console.WriteLine("Your Age: " + yourAge);
-            Console.WriteLine("Your Email: " + yourEmail);
-            Console.WriteLine("Points Program: " + assessmentPrograming);
-            Console.WriteLine("Points Mahth: " + assessmentMath);
-            Console.WriteLine("Points Physics: " + assessmentPhysics);
+            Double assessmentPhysics = 92.3; 
+            Console.WriteLine($"Full Name: {fullName}\n Your Age: {yourAge}\n Your Email {yourEmail}\n Your Points for Programming {assessmentPrograming}\n Your Points for Math {assessmentMath}\n Your Points for Physics {assessmentPhysics}" );
             Console.ReadKey();
+
+            //Console.WriteLine("Full name: " + fullName);
+            //Console.WriteLine("Your Age: " + yourAge);
+            //Console.WriteLine("Your Email: " + yourEmail);
+            //Console.WriteLine("Points Program: " + assessmentPrograming);
+            //Console.WriteLine("Points Mahth: " + assessmentMath);
+            //Console.WriteLine("Points Physics: " + assessmentPhysics);
+            //Console.ReadKey();
 
             //Начало второго задания
             Double a = assessmentPrograming;
             Double b = assessmentMath;
             Double c = assessmentPhysics;
 
-            Double totalPoints = a + b + c;
-            Console.WriteLine("Total Point: " + totalPoints);
+            //Double totalPoints = a + b + c;
+            Console.WriteLine($"Total ponts : {a+b+c}");
             Console.ReadKey();
 
-            Double averagePoints = (a + b + c) / 3;
-            Console.WriteLine("Average points: " + string.Format("{0:F2}", averagePoints));
+            //Double averagePoints = (a + b + c) / 3;
+            Console.WriteLine($"Average points: {(a+b+c)/3}");
             Console.ReadKey();
 
 
